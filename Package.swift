1571c446aa5dd34d0f443ce0666d3de44d2f68ed
Evil_Leenius <nlc@outlook.com>
2025-02-24 20:41:30 +0000

    Minimum macOS version raised from 12 to 13.3

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LLMTools",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13.3),
         .iOS(.v15)
     ],
     products: [
